af2f90aed8bb7b7558e745721dfc5d3b90cf3215
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-19 22:07:50 +0530

    user stored in db

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // We already have a record for given id
+          done(null, existingUser);
+        } else {
+          // We do not have any user with given id. Create one
+          new User({ googleId: profile.id }).save().then((user) => {
+            done(null, user);
+          });
+        }
+      });
     }
   )
 );
