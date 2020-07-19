5eb139af35c89946f3f383770931688ae27966ff
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-19 06:23:49 +0530

    oauth config

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(new GoogleStrategy());
 
 app.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}`);
