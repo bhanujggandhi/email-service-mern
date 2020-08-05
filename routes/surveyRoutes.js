4ce786df11fd9d9e639583204245f3a188c1c7af
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-05 18:25:54 +0530

    Mailer and template setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/email templates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -18,5 +20,8 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now(),
     });
+
+    //Send email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
