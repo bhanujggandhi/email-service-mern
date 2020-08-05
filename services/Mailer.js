931dbfd671dd39f0ae6159f4b88a851a97de79cc
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-05 18:45:59 +0530

    mailerobject setup

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,42 @@ const sendGrid = require('sendgrid');
 const helper = sendGrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSetting = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSetting.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSetting);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+
+    this.recipients.forEach((recipient) => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+}
 
 module.exports = Mailer;
