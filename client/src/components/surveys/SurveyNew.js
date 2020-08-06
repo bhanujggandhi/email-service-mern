250511d77742509d0e03d20f043fde720433e89c
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-07 00:17:21 +0530

    Redux-form setup

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,7 +1,14 @@
+// SurveyNew shows SurveyForm and SurveyFormReview
+
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 export default class SurveyNew extends Component {
   render() {
-    return <div>Survey new!</div>;
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
   }
 }
