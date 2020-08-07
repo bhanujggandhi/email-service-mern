6c10b0cdbdaa9447a3a05f03ef7def276741b1d4
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-08 04:46:53 +0530

    Validation added to form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const SurveyField = ({ input, label }) => {
+const SurveyField = ({ input, label, meta: { error, touched } }) => {
   //This field component is passed into Field component by Redux-form in SUrveyForm component
   //redux-form has provided no. of props to SurveyField component like onChange, onFocus, onBlur, etc.
   //props.input has all these properties.. so we've destructured it
-  console.log(input);
+  //meta property has various meta data having error field for form validation
+  // console.log(meta);
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
   //{...input} means we are spreading input object. It is equivalent to onBlur={input.onBlur} and so on.
