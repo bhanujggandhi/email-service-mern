27e6392862977b5c53e46a789eb4735e1abf3586
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-07 21:30:10 +0530

    Redux-form handleSubmit

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,11 +1,22 @@
 // SurveyForm shows a form for a user to add input
 
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
-    return <div>Survey Form!</div>;
+    return (
+      <div>
+        <form
+          // handleSubmit is a function provided by redux-form which is wired in the export statement
+          // handleSubmit takes a function which is automatically called when form is submitted
+          onSubmit={this.props.handleSubmit((values) => console.log(values))}
+        >
+          <Field type='text' name='surveyTitle' component='input' />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
