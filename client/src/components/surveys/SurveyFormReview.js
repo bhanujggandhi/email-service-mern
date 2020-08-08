d8eb77ee6d5c05c8f839a7fb3468299f840e4afb
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-08 19:09:24 +0530

    DRY form review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,23 @@
 // SurveyFormReview shows users their form inputs for review
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = (props) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{props.formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      {reviewFields}
       <button className='yellow darken-3 btn-flat' onClick={props.onCancel}>
         Back
       </button>
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -3,7 +3,6 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
-import formFields from './formFields';
 
 export default class SurveyNew extends Component {
   // constructor(props) {
