c8e28cbac76679a735fd68ed5aa8ab9173b8185d
bhanujggandhi <gandhibhanuj@gmail.com>
2020-08-12 21:24:01 +0530

    Email responses setup

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,30 +10,43 @@ const surveyTemplate = require('../services/email templates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.get('/api/surveys/feedback', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting');
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
+    // console.log(req.body);
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
-          return {
-            email,
-            surveyId: match.surveyId,
-            choice: match.choice,
-          };
+          return { email, surveyId: match.surveyId, choice: match.choice };
         }
       })
       .compact()
-      .uniqBy('email', 'survey')
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date(),
+          }
+        ).exec();
+        // This says, look at the Survey collection, update exactly one document which matches the criterial:-
+        // id: surveyId and recipients should have email this and has not responded yet
+        //After finding the document, increment the choice property, either 'yes' or 'no' by 1. 'yes' or 'no' should be optimized by mongoDB itself, that's why square brackets. ([] is not an array)
+        //Next, set the recipients property which is found ($). $ means recipient which is found above, it's responded to true.
+      })
       .value();
-
-    console.log(events);
-
     res.send({});
   });
 
